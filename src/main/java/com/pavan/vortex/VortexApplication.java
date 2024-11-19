c1280fb76a2c641f9d6bf455bb8bdba22e8db803
Pavan Sunny Thomas <pavan.s@thinkpalm.com>
2024-11-19 12:00:55 +0530

    first commit

---
+++ src/main/java/com/pavan/vortex/VortexApplication.java
@@ -0,0 +1,13 @@
+package com.pavan.vortex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VortexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VortexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavan/vortex/VortexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavan.vortex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VortexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
